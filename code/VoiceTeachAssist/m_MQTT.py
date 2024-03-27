569ffd32a24ef1b27987bcb879351ac1190aa35d
epal-aliartou <80651010+epal-aliartou@users.noreply.github.com>
2024-03-27 10:49:17 +0200

    Update m_MQTT.py

--- code/VoiceTeachAssist/m_MQTT.py
+++ code/VoiceTeachAssist/m_MQTT.py
@@ -22,15 +22,6 @@ def on_subscribe(client, userdata, mid, granted_qos, properties=None):# print wh
 
 client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5) # using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31  # userdata is user defined data of any type, updated by user_data_set()  # client_id is the given name of the client
 client.on_connect = on_connect
-
-# client.username_pw_set("mqtt_user", "mqtt_user1234")# enable TLS for secure connection# client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)# set username and password
-# client.connect("192.168.1.78", 1883)# connect to HiveMQ Cloud on port 8883 (default for MQTT)
-
 client.on_subscribe = on_subscribe # setting callbacks, use separate functions like above for better visibility
 # client.on_message = on_message
 client.on_publish = on_publish
-
-# client.subscribe("encyclopedia/#", qos=1)# subscribe to all topics of encyclopedia by using the wildcard "#"
-# client.publish("encyclopedia/temperature", payload="mqtt_neo", qos=1)# a single publish, this can also be done in loops, etc.
-
-# client.loop_forever()# loop_forever for simplicity, here you need to stop the loop manually# you can also use loop_start and loop_stop
